638c1ce77bfdd81357580df2a54373ca7b16d8fe
Alexandru Peptan <alexpeptan@yahoo.com>
2018-10-27 14:33:27 +0300

    Random movement + collision among own ships avoided. Of course, at this point I did not realize the ships are not moving simultaneously from one turn to another, but in the order of receiving orders. That still causes ships to crash, but much less often

--- MyBot.py
+++ MyBot.py
@@ -26,39 +26,63 @@ game = hlt.Game()
 # As soon as you call "ready" function below, the 2 second per turn timer will start.
 game.ready("MyPythonBot")
 
-# Now that your bot is initialized, save a message to yourself in the log file with some important information.
-#   Here, you log here your id, which you can always fetch from the game object by using my_id.
-logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
-
-""" <<<Game Loop>>> """
+explore = {}
 
 while True:
-    # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
-    #   running update_frame().
+    # Get the latest game state.
     game.update_frame()
     # You extract player metadata and the updated map metadata here for convenience.
     me = game.me
     game_map = game.game_map
 
-    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
-    #   end of the turn.
+    if game.turn_number == 1:
+        shipyardPos = game_map[me.shipyard].position
+
+    futurePos = []
+
+    # A command queue holds all the commands you will run this turn.
     command_queue = []
 
     for ship in me.get_ships():
-        # For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
-        #   Else, collect halite.
-        if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
-            command_queue.append(
-                ship.move(
-                    random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
-        else:
-            command_queue.append(ship.stay_still())
+        if ship.is_full:
+            explore[ship.id] = False
+        if ship.position == shipyardPos:
+            explore[ship.id] = True
 
-    # If the game is in the first 200 turns and you have enough halite, spawn a ship.
-    # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
-    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
-        command_queue.append(me.shipyard.spawn())
+        if explore[ship.id]:
+            if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10:
+
+                while True:
+                    direction = random.choice([Direction.West, Direction.North, Direction.East, Direction.South])
+                    candidatePos = ship.position.directional_offset(direction)
+
+                    if (candidatePos not in futurePos) and game_map[candidatePos].is_empty:
+                        futurePos.append(candidatePos)
+                        game_map[candidatePos].mark_unsafe(ship)
+                        break
+
+                command_queue.append(ship.move(direction))
+            else:
+                command_queue.append(ship.stay_still())
+                futurePos.append(ship.position)
+                game_map[ship.position].mark_unsafe(ship)
+        else:
+            direction = game_map.naive_navigate(ship, me.shipyard.position)
+            candidatePos = ship.position.directional_offset(direction)
+            if candidatePos in futurePos:
+                command_queue.append(ship.stay_still())
+                futurePos.append(ship.position)
+                game_map[ship.position].mark_unsafe(ship)
+            else:
+                command_queue.append(ship.move(direction))
+                futurePos.append(candidatePos)
+                game_map[candidatePos].mark_unsafe(ship)
+
+    # If you're on the first turn and have enough halite, spawn a ship.
+    # Don't spawn a ship if you currently have a ship at port, though.
+    if game.turn_number <= 175 and me.halite_amount >= constants.SHIP_COST and not game_map[
+        me.shipyard].is_occupied:  # 00 and game.turn_number % 10 == 0
+        command_queue.append(game.me.shipyard.spawn())
 
     # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
-
